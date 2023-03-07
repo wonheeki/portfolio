b0d5f0bd120ce75ee2a917c9ab4d5351ff50292e
wonheeki <105844632+wonheeki@users.noreply.github.com>
2023-03-08 00:27:31 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@ $(function(){
 	FirstSlide();
 	SecondSlide();
 	ThirdSlide();
+	FourthSlide();
 	
 	
 		
@@ -33,14 +34,14 @@ function FirstSlide(){
 	const slideCount = slideImg.length; // 슬라이드 개수
 	const prev = document.querySelector('#prev1'); //이전 버튼
 	const next = document.querySelector('#next1'); //다음 버튼
-	const slideWidth = 1000; //한개의 슬라이드 넓이
-	const slideMargin = 100; //슬라이드간의 margin 값
+	const slideWidth = 47; //한개의 슬라이드 넓이
+	const slideMargin = 1.5; //슬라이드간의 margin 값
 
 	//전체 슬라이드 컨테이너 넓이 설정
-	slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
+	slides.style.width = (slideWidth + slideMargin) * slideCount + 'vw';
 
 	function moveSlide(num) {
-	  slides.style.left = -num * 736 + 'px';
+	  slides.style.left = -num * 48.5 + 'vw';
 	  currentIdx = num;
 	}
 	prev.addEventListener('click', function () {
@@ -60,14 +61,14 @@ function SecondSlide(){
 	const slideCount = slideImg.length; // 슬라이드 개수
 	const prev = document.querySelector('#prev2'); //이전 버튼
 	const next = document.querySelector('#next2'); //다음 버튼
-	const slideWidth = 1000; //한개의 슬라이드 넓이
-	const slideMargin = 100; //슬라이드간의 margin 값
+	const slideWidth = 47; //한개의 슬라이드 넓이
+	const slideMargin = 1.5; //슬라이드간의 margin 값
 
 	//전체 슬라이드 컨테이너 넓이 설정
-	slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
+	slides.style.width = (slideWidth + slideMargin) * slideCount + 'vw';
 
 	function moveSlide(num) {
-	  slides.style.left = -num * 736 + 'px';
+	  slides.style.left = -num * 48.5 + 'vw';
 	  currentIdx = num;
 	}
 	prev.addEventListener('click', function () {
@@ -87,14 +88,14 @@ function ThirdSlide(){
 	const slideCount = slideImg.length; // 슬라이드 개수
 	const prev = document.querySelector('#prev3'); //이전 버튼
 	const next = document.querySelector('#next3'); //다음 버튼
-	const slideWidth = 1000; //한개의 슬라이드 넓이
-	const slideMargin = 100; //슬라이드간의 margin 값
+	const slideWidth = 47; //한개의 슬라이드 넓이
+	const slideMargin = 1.5; //슬라이드간의 margin 값
 
 	//전체 슬라이드 컨테이너 넓이 설정
-	slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
+	slides.style.width = (slideWidth + slideMargin) * slideCount + 'vw';
 
 	function moveSlide(num) {
-	  slides.style.left = -num * 736 + 'px';
+	  slides.style.left = -num * 48.5 + 'vw';
 	  currentIdx = num;
 	}
 	prev.addEventListener('click', function () {
@@ -104,6 +105,34 @@ function ThirdSlide(){
 	  if (currentIdx !== slideCount - 1) {
 	    moveSlide(currentIdx + 1);
 	  }
+	});
+}
+
+
+function FourthSlide(){
+	const slides = document.querySelector('#slides4'); //전체 슬라이드 컨테이너
+	const slideImg = document.querySelectorAll('#slides4 li'); //모든 슬라이드들
+	let currentIdx = 0; //현재 슬라이드 index
+	const slideCount = slideImg.length; // 슬라이드 개수
+	const prev = document.querySelector('#prev4'); //이전 버튼
+	const next = document.querySelector('#next4'); //다음 버튼
+	const slideWidth = 47; //한개의 슬라이드 넓이
+	const slideMargin = 1.5; //슬라이드간의 margin 값
+
+	//전체 슬라이드 컨테이너 넓이 설정
+	slides.style.width = (slideWidth + slideMargin) * slideCount + 'vw';
+
+	function moveSlide(num) {
+	  slides.style.left = -num * 48.5 + 'vw';
+	  currentIdx = num;
+	}
+	prev.addEventListener('click', function () {
+		if (currentIdx !== 0) moveSlide(currentIdx - 1);
+	});
+	next.addEventListener('click', function () {
+		if (currentIdx !== slideCount - 1) {
+			moveSlide(currentIdx + 1);
+		}
 	});
 }
 
